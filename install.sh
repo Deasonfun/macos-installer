d0e8812b3cf69c5c29eb05a72989c895bd39065c
Kyle <kyle@Kyles-MacBook-Pro.local>
2020-07-25 21:16:11 -0400

    Added brew installs to install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#Set up Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+#Homebrew stuff
+brew cask install discord
+brew cask install spotify
+brew cask install vscodium
+brew cask install iterm2
